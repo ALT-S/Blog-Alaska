5587f3ff294ecc0566e57484a9a603debc6e9415
ALT <alts.thomas@gmail.com>
2017-02-22 21:51:17 +0100

    Suppression des use dans Contact

--- src/ALT/AppBundle/Controller/ContactController.php
+++ src/ALT/AppBundle/Controller/ContactController.php
@@ -5,14 +5,7 @@ namespace ALT\AppBundle\Controller;
 use ALT\AppBundle\Entity\Contact;
 use ALT\AppBundle\Form\DemandeContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Validator\Constraints\Email;
 
 class ContactController extends Controller
 {
